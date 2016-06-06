738e6020e8093d00d3493d98001b92cb0aff3375
Michael Pascuzzi <gatorpazz@gmail.com>
2016-06-06 14:41:20 -0400

    more JS

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
 var view = {
   displayMessage: function(msg) {
-
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
   },
   displayHit: function(location) {
 
