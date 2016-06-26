37e6d792c66359a0a3869fedeb60d1117cbabd05
Michael Pascuzzi <gatorpazz@gmail.com>
2016-06-25 20:35:22 -0400

    Added finished game message

--- battleship.js
+++ battleship.js
@@ -55,10 +55,38 @@ var model = {
 
 var controller = {
   guesses: 0,
-
   processGuess: function(guess) {
-    if (guess === null || guess.length !== 2) {
-      alert("Oops please enter a number or a letter that are on the board");
+    var location = parseGuess(guess);
+    if (location) {
+      this.guesses++
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships in " + this.guesses + " guesses");
+      }
     }
   }
 };
+
+function parseGuess(guess) {
+  var alphabet = ["A","B","C","D","E","F","G"];
+  if (guess === null || guess.length !== 2) {
+    alert("Oops please enter a number or a letter that are on the board");
+  } else {
+    firstChar = guess.charAt(0);
+    var row = alphabet.indexOf(firstChar);
+    var column = guess.charAt(1);
+    if (isNaN(row) || isNaN(column)) {
+      alert ("Oops that's not on the board!");
+    } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert ("Oops that's off the board!");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+
+controller.processGuess("A0");
+controller.processGuess("A6"); controller.processGuess("B6"); controller.processGuess("C6");
+controller.processGuess("C4"); controller.processGuess("D4"); controller.processGuess("E4");
+controller.processGuess("B0"); controller.processGuess("B1"); controller.processGuess("B2");
